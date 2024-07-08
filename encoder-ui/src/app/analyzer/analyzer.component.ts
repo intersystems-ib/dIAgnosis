435381271ab21073b90abec328769d21fd05e41a
isc-lperezra <lperezra@intersystems.com>
2024-07-08 17:06:44 +0200

    Analysis of text finished

--- encoder-ui/src/app/analyzer/analyzer.component.ts
+++ encoder-ui/src/app/analyzer/analyzer.component.ts
@@ -1,20 +1,36 @@
 import { Component } from '@angular/core';
 import { ReactiveFormsModule, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
 import { IrisService } from '../services/iris.service';
+import { NgbProgressbarModule, NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+
+interface Diagnostic {
+  code: String;
+  description: String;
+  similarity: number;
+}
+
+interface TextAndDiagnostic {
+  rawText: String;
+  diagnostics: Array<Diagnostic>;
+}
 
 @Component({
   selector: 'app-analyzer',
-  standalone: true,
-  imports: [ReactiveFormsModule],
   templateUrl: './analyzer.component.html',
   styleUrl: './analyzer.component.scss'
 })
+
 export class AnalyzerComponent {
 
   textUpdated = "";
-  diagnostics: Array<any> = []
+  diagnostics: Array<any> = [];
+  loading = false;
+  error = false;
+  totalReceived = 0;
+  textAndDiagnosticList: Array<TextAndDiagnostic> = [];
 
-  constructor(private irisService: IrisService) {
+  constructor(private irisService: IrisService) 
+  {
     
   }
 
@@ -27,24 +43,65 @@ export class AnalyzerComponent {
   }
 
   onSubmit() {
-    const textData = {
-      "Text": this.textToAnalyze?.value
-    };
+    this.diagnostics = [];
     var textHTML = this.textToAnalyze?.value;
-
-    this.irisService.analyzeText(textData).subscribe({next: res =>{
-      this.diagnostics = res;
-      this.diagnostics.forEach((diagnostic) => {
-        const phrase = textHTML.match(new RegExp(diagnostic.RawText.split(" ")[0] + "(.*)" +diagnostic.RawText.split(" ")[2],"ig"))[0];
-          textHTML = textHTML.replace(phrase,"<mark>"+phrase+"</mark>");
-      })
-      this.textUpdated = textHTML
-    },
-    error: err => {
-      console.error(JSON.stringify(err));
-    }
-    })
-  }
+    var textOriginal = textHTML;
+    var textToProcess = this.textToAnalyze?.value.split(".");
+    var forReading = 100/(textToProcess.length-1);
+    this.totalReceived = 0;
+    this.error = false;
+    this.loading = true;
+    this.textAndDiagnosticList = [];
+    for (var index in textToProcess){
+      if (textToProcess[index] !== "")
+      {
+        const textData = {
+          "Text": textToProcess[index]
+        };
+        this.irisService.analyzeText(textData).subscribe({next: res =>{
+          if (res.length > 0){
+            this.diagnostics = this.diagnostics.concat(res);
+          }
+          this.totalReceived += forReading;
+          if (this.totalReceived + 1 >= 100){
+            this.diagnostics.forEach((diagnostic, indexDiag) => {              
+              let phrase = "";
+              if (diagnostic.RawText.split(" ").length == 3){
+                phrase = textOriginal.match(new RegExp(diagnostic.RawText.split(" ")[0] + "(.{0,100})" +diagnostic.RawText.split(" ")[2],"ig"))[0];
+              }
+              else if (diagnostic.RawText.split(" ").length == 2){
+                phrase = textOriginal.match(new RegExp(diagnostic.RawText.split(" ")[0] + "(.{0,100})" +diagnostic.RawText.split(" ")[1],"ig"))[0];
+              }
+              else if (diagnostic.RawText.split(" ").length == 4){
+                phrase = textOriginal.match(new RegExp(diagnostic.RawText.split(" ")[0] + "(.{0,100})" +diagnostic.RawText.split(" ")[3],"ig"))[0];
+              }
+              else if (diagnostic.RawText.split(" ").length == 1){
+                phrase = textOriginal.match(diagnostic.RawText);
+              }
+              var indexDiag = this.textAndDiagnosticList.findIndex(obj => obj.rawText == phrase);
+              const diagnosticEncoded: Diagnostic = {code: diagnostic.CodeId, description: diagnostic.Description, similarity: diagnostic.Similarity}
+              if (indexDiag == -1)
+              {
+                textHTML = textHTML.replace(phrase,"<mark>"+phrase+"</mark>");
+                let textAndDiagnostic: TextAndDiagnostic = {rawText: phrase, diagnostics: []};
+                textAndDiagnostic.diagnostics.push(diagnosticEncoded);
+                this.textAndDiagnosticList.push(textAndDiagnostic);
+              }
+              else {
+                this.textAndDiagnosticList[indexDiag].diagnostics.push(diagnosticEncoded)
+              }                           
+            })
+            this.textUpdated = textHTML
+            this.loading = false;
+          }
+        },
+        error: err => {
+          this.error = true;
+        }
+        }); 
+      }
+  }  
+}
 
   createAlert() {
     alert("Es un diagnóstico");
--- encoder-ui/src/app/app.module.ts
+++ encoder-ui/src/app/app.module.ts
@@ -3,24 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NgbModule, NgbPaginationModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbAccordionModule, NgbModule, NgbPaginationModule, NgbProgressbarModule, NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 import { DiagnosisComponent } from './diagnosis/diagnosis.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CodelistComponent } from './codelist/codelist.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AnalyzerComponent } from './analyzer/analyzer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiagnosisComponent,
-    CodelistComponent
+    CodelistComponent,
+    AnalyzerComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     NgbPaginationModule,
+    NgbProgressbarModule,
+    NgbAccordionModule,
     NgbTypeaheadModule,
+    ReactiveFormsModule,
     FormsModule,
     HttpClientModule
   ],
